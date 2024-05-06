30006f20672e11cb7868dd417a3083ebe4c7b6ff
Andrzej Rumak <ajgltl@gmail.com>
2024-05-06 12:14:48 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const { loggedIn } = useUserSession()
+</script>
+
+<template>
+  <UCard>
+    <template #header>
+      <h3 class="text-lg font-semibold leading-6">
+        Todo List
+      </h3>
+      <UButton
+        v-if="!loggedIn"
+        to="/api/auth/github"
+        icon="i-simple-icons-github"
+        label="Login with GitHub"
+        color="black"
+        external
+      />
+      <UButton
+        v-else
+        to="/todos"
+        icon="i-heroicons-list-bullet"
+        label="Go to Todos"
+        color="black"
+      />
+    </template>
+    <p class="font-medium">
+      Welcome to Nuxt Todos Edge.
+    </p>
+    <p>
+      Test deployment the Nuxt Todos Edge on Vercel
+    </p>
+    <hr class="dark:border-gray-700">
+    <p class="text-sm text-gray-700 dark:text-gray-300 italic">
+      No personal informations regarding your GitHub account are stored in database.<br>
+      We store only the todos created linked with your GitHub ID.
+    </p>
+  </UCard>
+</template>
